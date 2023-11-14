2f72ec493c2033be57a11a7e8bcca7015f256562
jieDeng <jiedeng0079@gmail.com>
2023-11-14 14:10:35 -0600

    [NO JIRA] make new class

--- src/main/java/com/example/rewardsprogram/model/ErrorResponse.java
+++ src/main/java/com/example/rewardsprogram/model/ErrorResponse.java
@@ -1,4 +1,12 @@
 package com.example.rewardsprogram.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ErrorResponse {
+    private int errorCode;
+    private String message;
 }
