6da998868f59d6ac3188a79bd21e314a27e4197d
jieDeng <jiedeng0079@gmail.com>
2023-11-13 19:17:33 -0600

    [NO JIRA] Move entity file

--- src/main/java/com/example/rewardsprogram/entity/Customer.java
+++ src/main/java/com/example/rewardsprogram/entity/Customer.java
@@ -1,4 +1,4 @@
-package entity;
+package com.example.rewardsprogram.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
--- src/main/java/com/example/rewardsprogram/entity/Transaction.java
+++ src/main/java/com/example/rewardsprogram/entity/Transaction.java
@@ -1,4 +1,4 @@
-package entity;
+package com.example.rewardsprogram.entity;
 
 
 import jakarta.persistence.*;
