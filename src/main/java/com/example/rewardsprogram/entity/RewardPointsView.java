88dc5d9d0ad883e1a4c6437fd1f0a3dbb000c22e
jieDeng <jiedeng0079@gmail.com>
2023-11-13 20:58:24 -0600

    [NO JIRA] Fix "ID" to "Id" and the entity name

--- src/main/java/com/example/rewardsprogram/entity/RewardPointsView.java
+++ src/main/java/com/example/rewardsprogram/entity/RewardPointsView.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 @Getter
 
-@Entity(name = "RewardPoint")
+@Entity(name = "RewardPointsView")
 @Immutable
 // Marks the entity as immutable. Hibernate will not try to write any changes to this entity.
 @Table(name = "RewardPointsView", uniqueConstraints = {
@@ -20,11 +20,11 @@ public class RewardPointsView {
 
     @Id
     @Column(name = "TransactionID")
-    private Long transactionID;
+    private Long transactionId;
 
 
     @Column(name = "CustomerID")
-    private Long customerID;
+    private Long customerId;
 
 
     @Column(name = "TransactionDate")
