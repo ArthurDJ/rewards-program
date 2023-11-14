99bac25b36524a511da8949f2a080364f1dc092f
jieDeng <jiedeng0079@gmail.com>
2023-11-13 20:26:25 -0600

    [NO JIRA] Build Transaction Repository

---
+++ src/main/java/com/example/rewardsprogram/repository/TransactionRepository.java
@@ -0,0 +1,31 @@
+package com.example.rewardsprogram.repository;
+
+import com.example.rewardsprogram.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    // Find transactions by customer ID
+    List<Transaction> findAllByCustomerID(Long customerID);
+
+    // Find transactions by  transaction ID
+    Transaction findByTransactionID(Long transactionID);
+
+    // Find transactions within a date range
+    List<Transaction> findALLByTransactionDateBetween(Date startDate, Date endDate);
+
+    // Find transactions on a specific date
+    List<Transaction> findAllByTransactionDate(Date transactionDate);
+
+    // Find transactions before a specific date
+    List<Transaction> findAllByTransactionDateBefore(Date beforeDate);
+
+    // Find transactions after a specific date
+    List<Transaction> findAllByTransactionDateAfter(Date afterDate);
+
+}
