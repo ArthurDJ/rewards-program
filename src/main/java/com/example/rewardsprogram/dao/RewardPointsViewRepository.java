3d03a34a31ab28955ac5594081944fbe05bb447a
jieDeng <jiedeng0079@gmail.com>
2023-11-14 15:42:51 -0600

    [NO JIRA] remove unused package

--- src/main/java/com/example/rewardsprogram/dao/RewardPointsViewRepository.java
+++ src/main/java/com/example/rewardsprogram/dao/RewardPointsViewRepository.java
@@ -2,11 +2,9 @@ package com.example.rewardsprogram.dao;
 
 
 import com.example.rewardsprogram.entity.RewardPointsViewEntity;
-import com.example.rewardsprogram.entity.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
