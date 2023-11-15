a3a48afa75fae2edb83088f62163488b2a80d2e3
jieDeng <jiedeng0079@gmail.com>
2023-11-15 11:43:26 -0600

    [NO JIRA] Build RewardPointsViewService and RewardPointsViewController.

---
+++ src/main/java/com/example/rewardsprogram/controller/RewardPointsViewController.java
@@ -0,0 +1,5 @@
+package com.example.rewardsprogram.controller;
+
+public class RewardPointsViewController {
+
+}
---
+++ src/main/java/com/example/rewardsprogram/service/RewardPointsViewService.java
@@ -0,0 +1,4 @@
+package com.example.rewardsprogram.service;
+
+public interface RewardPointsViewService {
+}
