7ed3d60ce52ee273a647bfc635c9adabf3b06f7d
jieDeng <jiedeng0079@gmail.com>
2023-11-14 18:15:56 -0600

    [NO JIRA] Add interface CustomerService.

---
+++ src/main/java/com/example/rewardsprogram/service/CustomerService.java
@@ -0,0 +1,33 @@
+package com.example.rewardsprogram.service;
+
+
+import com.example.rewardsprogram.model.Customer;
+
+import java.util.Date;
+import java.util.List;
+
+public interface CustomerService {
+
+    Customer findCustomerById(Long customerId);
+
+    List<Customer> findCustomersByFirstName(String firstName);
+
+    List<Customer> findCustomersByLastName(String lastName);
+
+    Customer findCustomerByEmail(String email);
+
+    List<Customer> findCustomersByFirstNameStartingWith(String prefix);
+
+    List<Customer> findCustomersByDateOgBirth(Date dateOfBirth);
+
+    List<Customer> findCustomersByPhone(String Phone);
+
+    Customer saveOrUpdateCustomer(Customer customer);
+
+    void deleteCustomerByCustomerId(Long customerId);
+
+    List<Customer> findAllCustomers();
+
+
+
+}
